1142840ccbb99d692dde04d61cc44cad340df98e
rixcx <adts0415.ovo@gmail.com>
2025-05-03 11:43:36 +0900

    [members] create edit delete members function

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -16,7 +16,29 @@ class MembersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
   
+  def update
+    @member = Member.find(params[:id])
+
+    if @member.update(member_params)
+      redirect_to members_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+   @member = Member.find(params[:id])
+   
+    if @member.destroy
+      redirect_to members_path
+    end
+  end
+
   private
   def member_params # スコープ内で使うパラメータ定義
     params.require(:member).permit(:name, :grade, :classroom, :age, :text)
